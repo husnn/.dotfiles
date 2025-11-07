06f49821fdc6da9515259b401310028218cd6219
Husnain Javed <me@husnainjaved.com>
2025-11-07 17:13:59 +0000

    Map <leader>w to save current buffer

--- nvim/.config/nvim/lua/husnn/core/keymaps.lua
+++ nvim/.config/nvim/lua/husnn/core/keymaps.lua
@@ -8,6 +8,8 @@ keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally' })
 keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' })
 keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
 
+keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = 'Save current buffer' })
+
 keymap.set('n', 'x', '"_x', { desc = 'Delete char without copying' })
 keymap.set('n', 'X', '"_d', { desc = 'Delete command without copying' })
 
